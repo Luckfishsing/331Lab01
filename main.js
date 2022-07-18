fd4b243b6b4116244b8c53b2c9b31e92e09b63cd
haitao.l <lht9977outlook.com>
2022-07-18 08:58:18 +0800

    up3.6

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             productDes: 'We have two kinds of socks, green and purple.',
-            image: './assets/images/socks_green.jpg'
+            image: './assets/images/socks_green.jpg',
+            Link: 'http://www.camt.cmu.ac.th'
         }
         
     }
