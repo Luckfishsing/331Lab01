204778420e9162c3f818f51abee0dec2e2afccac
haitao.l <lht9977outlook.com>
2022-07-18 09:27:57 +0800

    update4.9

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
+            product: 'Shoes',
             productDes: 'We have two kinds of socks, green and purple.',
             image: './assets/images/socks_green.jpg',
-            Link: 'http://www.camt.cmu.ac.th'
+            Link: 'http://www.camt.cmu.ac.th',
+            inStock: true,
+            inventory: 100,
+            Onsale: true,
         }
         
     }
