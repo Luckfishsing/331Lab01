4d110dfb40b02b44b0e1fb9018a016c64bb11293
haitao.l <lht9977outlook.com>
2022-07-18 10:07:35 +0800

    update56

--- main.js
+++ main.js
@@ -8,8 +8,31 @@ const app = Vue.createApp({
             inStock: true,
             inventory: 100,
             Onsale: true,
+            count:10,
+            details:['50% cotton','30% wool','20% polyester'],
+            variants: [
+                { id:2234,color: 'green' ,image: './assets/images/socks_green.jpg'},
+                { id:2235,color: 'blue' ,image: './assets/images/socks_blue.jpg'}
+            ],
+            sizes: [
+                { id:1,size:'S  '},
+                { id:2,size:'M  '},
+                { id:3,size:'L  '}
+            ],
+            cart:0
         }
         
+    },
+    methods: {
+        addToCart(){
+            this.cart = this.cart + 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        ToggleStockStatus(){
+            this.inStock = !this.inStock
+        }
     }
 })
 
